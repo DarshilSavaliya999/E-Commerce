1c01699f36a3abca43d2409424966cc96c337712
DarshilSavaliya <122202933+DarshilSavaliya999@users.noreply.github.com>
2023-07-12 22:33:15 +0530

    till video 37 (12-07-2023)

--- Bulky/Data/ApplicationDbContext.cs
+++ Bulky/Data/ApplicationDbContext.cs
@@ -11,5 +11,16 @@ namespace Bulky.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = "1" },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = "2" },
+                new Category { Id = 3, Name = "History", DisplayOrder = "3" },
+                new Category { Id = 4, Name = "Love", DisplayOrder = "4" },
+                new Category { Id = 5, Name = "Erotic", DisplayOrder = "5" }
+                );
+        }
     }
 }
