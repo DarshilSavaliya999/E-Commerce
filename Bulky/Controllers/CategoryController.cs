a7f45af01db4d59c20ebbebe889fd4d25d9082f9
DarshilSavaliya <122202933+DarshilSavaliya999@users.noreply.github.com>
2023-07-29 11:40:33 +0530

    till video 77 (29-07-2023)

--- Bulky/Controllers/CategoryController.cs
+++ Bulky/Controllers/CategoryController.cs
@@ -7,13 +7,13 @@ namespace Bulky.Controllers
 {
     public class CategoryController : Controller
     {
-        private readonly ICategoryRepository _categoryRepo;
-        public CategoryController(ICategoryRepository Db) {
-            _categoryRepo=Db;
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoryController(IUnitOfWork unitOfWork) {
+            _unitOfWork=unitOfWork;
         }
         public IActionResult Index()
         {
-            List<Category> objCategoryList = _categoryRepo.GetAll().ToList();
+            List<Category> objCategoryList = _unitOfWork.Category.GetAll().ToList();
             return View(objCategoryList);
         }
         public IActionResult Create()
@@ -29,8 +29,8 @@ namespace Bulky.Controllers
             }
             if (ModelState.IsValid)
             {
-                _categoryRepo.Add(obj);
-                _categoryRepo.Save();
+                _unitOfWork.Category.Add(obj);
+                _unitOfWork.Save();
                 TempData["success"] = "Category Created Successfully.";
                 return RedirectToAction("Index");
             }
@@ -44,7 +44,7 @@ namespace Bulky.Controllers
                 return NotFound();
             }
 
-            Category? categoryFromDb = _categoryRepo.Get(u=>u.Id==id);
+            Category? categoryFromDb = _unitOfWork.Category.Get(u=>u.Id==id);
 
             if (categoryFromDb == null)
             {
@@ -57,8 +57,8 @@ namespace Bulky.Controllers
         {
             if (ModelState.IsValid)
             {
-                _categoryRepo.Update(obj);
-                _categoryRepo.Save();
+                _unitOfWork.Category.Update(obj);
+                _unitOfWork.Save();
                 TempData["success"] = "Category Edited Successfully.";
                 return RedirectToAction("Index");
             }
@@ -72,7 +72,7 @@ namespace Bulky.Controllers
                 return NotFound();
             }
 
-            Category? categoryFromDb = _categoryRepo.Get(u=>u.Id==id);
+            Category? categoryFromDb = _unitOfWork.Category.Get(u=>u.Id==id);
 
             if (categoryFromDb == null)
             {
@@ -83,14 +83,14 @@ namespace Bulky.Controllers
         [HttpPost,ActionName("Delete")]
         public IActionResult DeletePOST(int? id)
         {
-            Category? obj = _categoryRepo.Get(u=>u.Id==id);
+            Category? obj = _unitOfWork.Category.Get(u=>u.Id==id);
 
             if (obj == null)
             {
                 return NotFound();
             }
-            _categoryRepo.Remove(obj);
-            _categoryRepo.Save();
+            _unitOfWork.Category.Remove(obj);
+            _unitOfWork.Save();
             TempData["success"] = "Category Deleted Successfully.";
             return RedirectToAction("Index");
         }
