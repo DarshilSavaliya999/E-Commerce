d2d4ec32ef38417d53cc1e86f1e80b742c06af09
DarshilSavaliya <122202933+DarshilSavaliya999@users.noreply.github.com>
2023-08-12 14:35:15 +0530

    till video 102 (12-08-2023)

--- Bulky/wwwroot/js/product.js
+++ Bulky/wwwroot/js/product.js
@@ -8,8 +8,22 @@ function loadDataTable() {
             { data: 'Title' ,"width":"25%"},
             { data: 'ISBN', "width": "15%" },
             { data: 'ListPrice', "width": "10%" },
-            { data: 'Author', "width": "20%" },
-            { data: 'Category.Name', "width": "15%" }
+            { data: 'Author', "width": "15%" },
+            { data: 'Category.Name', "width": "10%" },
+            {
+                data: 'Id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                                <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2">
+                                    <i class="bi bi-pencil-square"></i>  Edit
+                                </a>
+                                <a href="" class="btn btn-danger mx-2">
+                                    <i class="bi bi-trash-fill"></i>  Delete
+                                </a>
+                            </div>`
+                },
+                "width": "25%"
+            }
         ]
     });
 }
