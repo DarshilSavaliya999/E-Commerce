2cc479cc3aa7d81af2d423072b60e25459433b74
DarshilSavaliya <122202933+DarshilSavaliya999@users.noreply.github.com>
2023-08-14 17:01:26 +0530

    till video 117 (14-08-2023)

--- Bulky/Areas/Admin/Controllers/ProductController.cs
+++ Bulky/Areas/Admin/Controllers/ProductController.cs
@@ -2,6 +2,8 @@
 using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
 using Bulky.Models.ViewModels;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
@@ -9,6 +11,7 @@ using Newtonsoft.Json;
 namespace Bulky.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
     public class ProductController : Controller
     {
         private readonly IWebHostEnvironment _webHostEnvironment;
