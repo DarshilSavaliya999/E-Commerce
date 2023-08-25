fd0f53c651df3c23b34ee3d70498ab1c85910b2b
DarshilSavaliya <122202933+DarshilSavaliya999@users.noreply.github.com>
2023-08-25 11:36:10 +0530

    till video 134 (25-08-2023)

---
+++ Bulky/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bulky.Areas.Customer.Controllers
+{
+    [Area("customer")]
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
